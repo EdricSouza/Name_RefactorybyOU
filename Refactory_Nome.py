704b5de19e7ae81d198312b9bdb8461d1c902d53
Edric Guilherme Bahia de Souza <49295083+EdricSouza@users.noreply.github.com>
2024-09-06 14:55:20 -0300

    Update Refactory_Nome.py
    Atualização do código

--- Refactory_Nome.py
+++ Refactory_Nome.py
@@ -2,18 +2,34 @@ import os
 import subprocess
 import time
 
+#Criando variáveis
 exe = "powershell.exe"
 comando = "(Get-ADComputer $env:COMPUTERNAME).DistinguishedName"
 
+#Comando no powershell
 retorno = subprocess.run(
     args=[exe, comando],
     capture_output=True,
     shell= True,
     universal_newlines=True)
 
-print("Retorno: ", retorno.stdout)
-print("Erro: ", retorno.stderr)
-print("Returno do código: ", retorno.returncode)
-
-
+if(retorno.returncode == 0):
+    #Retorno de valores
+    print("Retorno: ", retorno.stdout)
+    print("Erro: ", retorno.stderr)
+    
+    #Identificação da OU
+    inicio = retorno.stdout.index("OU=") + 3
+    fim = retorno.stdout.index(",", inicio + 1)
+    
+    OU = retorno.stdout[inicio:fim]
+    
+    print(OU)
+    
+    #Casos
+    match OU:
+        case "TI":
+            print("Você faz parte do TI")
+        case "Administrador":
+            print("Você faz parte do Administrador")
 
